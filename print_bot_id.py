3a11069e7f95185c9f2edcdfaade7231f98d1991
Eric Dorsey <dorseye@gmail.com>
2016-06-26 09:51:32 -0600

    Error handling for error in API call

--- print_bot_id.py
+++ print_bot_id.py
@@ -15,5 +15,7 @@ if __name__ == "__main__":
         for user in users:
             if 'name' in user and user.get('name') == BOT_NAME:
                 print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+    elif "error" in api_call:
+        print("There was an error in the API call: {0}".format(api_call["error"]))
     else:
         print("could not find bot user with the name " + BOT_NAME)
